b8c0ef7ad885e44639d808783cfdf91bcee75627
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-10-03 14:52:25 +0300

    Step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,6 +5,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
@@ -14,7 +15,12 @@ public class App implements Callable<Integer> {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
-
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private File filepath1;
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private File filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format = "";
     @Override
     public Integer call() throws Exception { // your business logic goes here...
         System.out.println("Hello World!");
