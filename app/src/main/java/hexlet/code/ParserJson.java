20c8b23ddeed5e6a6a80070c91fa5dc22dc86b59
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-10-24 17:38:56 +0300

    final to ParserJson & ParserYml

--- app/src/main/java/hexlet/code/ParserJson.java
+++ app/src/main/java/hexlet/code/ParserJson.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParserJson implements Parser {
-    public Map<String, Object> parse(String fileContent) throws JsonProcessingException {
+    public final Map<String, Object> parse(String fileContent) throws JsonProcessingException {
         Map<String, Object> fileMap = new HashMap<>();
         ObjectMapper objectMapper = new ObjectMapper();
         fileMap = objectMapper.readValue(fileContent, Map.class);
--- app/src/main/java/hexlet/code/ParserYml.java
+++ app/src/main/java/hexlet/code/ParserYml.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParserYml implements Parser {
-    public Map<String, Object> parse(String fileContent) throws JsonProcessingException {
+    public final Map<String, Object> parse(String fileContent) throws JsonProcessingException {
         Map<String, Object> fileMap = new HashMap<>();
         ObjectMapper objectMapper = new YAMLMapper();
         fileMap = objectMapper.readValue(fileContent, Map.class);
