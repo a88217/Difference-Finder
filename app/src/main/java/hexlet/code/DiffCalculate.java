67180b0de2a2bac9e6050b370af895e0073d3cb1
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-10-24 21:21:04 +0300

    Calculate

--- app/src/main/java/hexlet/code/DiffCalculate.java
+++ app/src/main/java/hexlet/code/DiffCalculate.java
@@ -1,40 +1,34 @@
 package hexlet.code;
 
-import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class DiffCalculate {
 
     public static Map<String, List> calculate(Map<String, Object> mapFile1, Map<String, Object> mapFile2) {
 
         SortedMap<String, List> resultMap = new TreeMap<String, List>();
-        mapFile1.entrySet().stream()
-                .forEach(entry -> {
-                    if (mapFile2.containsKey(entry.getKey())) {
-                        if (mapFile2.get(entry.getKey()) != null && entry.getValue() != null) {
-                            if (mapFile2.get(entry.getKey()).equals(entry.getValue())) {
-                                resultMap.put(entry.getKey(), Arrays.asList("Unchanged", entry.getValue()));
-                            } else {
-                                resultMap.put(entry.getKey(),
-                                        Arrays.asList("Modified", entry.getValue(), mapFile2.get(entry.getKey())));
-                            }
-                        } else if (mapFile2.get(entry.getKey()) == null && entry.getValue() == null) {
-                            resultMap.put(entry.getKey(), Arrays.asList("Unchanged", null));
+        Set<String> keySet = new HashSet<>();
+        keySet.addAll(mapFile1.keySet());
+        keySet.addAll(mapFile2.keySet());
+        keySet.stream()
+                .forEach(key -> {
+                    if (mapFile1.containsKey(key) && mapFile2.containsKey(key)) {
+                        if (Objects.equals(mapFile1.get(key), mapFile2.get(key))) {
+                            resultMap.put(key, Arrays.asList("Unchanged", mapFile1.get(key)));
                         } else {
-                            resultMap.put(entry.getKey(),
-                                    Arrays.asList("Modified", entry.getValue(), mapFile2.get(entry.getKey())));
+                            resultMap.put(key, Arrays.asList("Modified", mapFile1.get(key), mapFile2.get(key)));
                         }
+                    } else if (mapFile1.containsKey(key) && !mapFile2.containsKey(key)) {
+                        resultMap.put(key, Arrays.asList("Deleted", mapFile1.get(key)));
                     } else {
-                        resultMap.put(entry.getKey(), Arrays.asList("Deleted", entry.getValue()));
-                    }
-                });
-        mapFile2.entrySet().stream()
-                .forEach(entry -> {
-                    if (!mapFile1.containsKey(entry.getKey())) {
-                        resultMap.put(entry.getKey(), Arrays.asList("Added", entry.getValue()));
+                        resultMap.put(key, Arrays.asList("Added", mapFile2.get(key)));
                     }
                 });
         return resultMap;
